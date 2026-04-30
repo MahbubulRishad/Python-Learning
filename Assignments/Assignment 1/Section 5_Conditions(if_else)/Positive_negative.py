db6237e59d77d29978a8cda0fa86d4967205c682
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-01 01:01:11 +0600

    If else

---
+++ Assignments/Assignment 1/Section 5_Conditions(if_else)/Positive_negative.py
@@ -0,0 +1,11 @@
+num = int(input("Enter a number: "))
+if num < 0:
+    print(f"{num}, the number is negative.")
+
+elif num >0:
+    print(f"{num}, the number is positive.")
+
+else:
+    print(f"{num}, the number is zero.")
+
+
