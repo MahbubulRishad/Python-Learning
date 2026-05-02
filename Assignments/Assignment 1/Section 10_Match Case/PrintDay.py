510ca2e7fb95f9789a62cbe53a4d473ac8689ca9
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-03 00:52:39 +0600

    Match-Day print

---
+++ Assignments/Assignment 1/Section 10_Match Case/PrintDay.py
@@ -0,0 +1,18 @@
+inputDay = int(input("Please enter a number: (1-7) "))
+match inputDay:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:
+        print("Please enter a valid number")
