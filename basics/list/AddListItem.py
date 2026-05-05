160884e5b8b5bc3d0982fa95d77c7ee950736d54
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-05 23:24:20 +0600

    Add list item

---
+++ basics/list/AddListItem.py
@@ -0,0 +1,14 @@
+programming = ['Python', 'Java', 'C++']
+print(programming)
+
+programming.append('GOOGLE')
+print(programming)
+
+programming.insert(3, "CSS")
+print(programming)
+
+new_programming = ['Ruby', 'TypeScript']
+print(new_programming)
+
+programming.extend(new_programming)
+print(programming)
