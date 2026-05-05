c1646bdf4be1237b9d0874c06f4e458f1a7b6ba0
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-06 00:21:26 +0600

    Copy list

--- basics/list/AddListItem.py
+++ basics/list/AddListItem.py
@@ -12,3 +12,19 @@ print(new_programming)
 
 programming.extend(new_programming)
 print(programming)
+
+numbers = [1, 2, 3, 4, 5, 6]
+
+#join 2 list - using loop
+for items in numbers:
+    programming.append(items)
+print(programming)
+
+#copy list
+no1 = [1, 2, 3]
+no2 = [4, 5, 6]
+no3 = [7, 8, 9]
+no_copy = no2.copy()
+print(no1)
+print(no_copy)
+
