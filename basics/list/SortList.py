4eb9a92fac47dfb40681a17219bcaec40aa2bc46
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-06 00:10:37 +0600

    Sort in list

---
+++ basics/list/SortList.py
@@ -0,0 +1,13 @@
+programmings = ['Javascript', 'Typescript', 'Python','c#', 'Html', 'Css', 'Java']
+programmings.sort()
+print(programmings)
+
+programmings.sort(reverse=True)
+print(programmings)
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+numbers.sort()
+print(numbers)
+
+numbers.sort(reverse=True)
+print(numbers)
