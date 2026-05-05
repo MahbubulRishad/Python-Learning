371b25bc1c997ad4013969eef5521c18a2adc782
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-05 23:30:11 +0600

    Change list item

---
+++ basics/list/ChangeListItems.py
@@ -0,0 +1,7 @@
+programming = ["Python", "Java", "C++", "Go", "HTML"]
+
+programming[0] = "C#"
+print(programming)
+
+programming[1:3] = ["Javascript", "Typescript"] #index,end-1
+print(programming)
