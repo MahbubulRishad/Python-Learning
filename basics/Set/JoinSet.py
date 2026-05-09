428f1b63f4db316f4bbfa4b782f89cddb69c6518
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-09 15:09:39 +0600

    join two sets

---
+++ basics/Set/JoinSet.py
@@ -0,0 +1,11 @@
+languages1 = {'Python', 'Java', 'C++', 'TypeScript', 'C#', 'LaTex', 'HTML', 'CSS', 'Ruby'}
+languages2 = {'Bangla', 'English', 'Telegu', 'Marathi', 'Python', 'Java'}
+
+union_Set = languages1.union(languages2)
+print(union_Set)
+
+intersection_Set = languages1.intersection(languages2)
+print(intersection_Set)
+
+difference_Set = languages1.difference(languages2)
+print(difference_Set)
