65d5f4a43930cfbf41d1154591e3aa0cac090c2a
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-09 11:20:58 +0600

    Access in set

---
+++ basics/Set/AccessSetItems.py
@@ -0,0 +1,18 @@
+languages = {"Python", "C++", "Java", "TypeScript"}
+print(languages)
+
+#looping
+for language in languages:
+    print(language)
+
+#value present in set
+if "Python" in languages:
+    print("Present")
+else:
+    print("Not present")
+
+#value not present in set
+if "Go" in languages:
+    print("Present")
+else:
+    print("Not present")
