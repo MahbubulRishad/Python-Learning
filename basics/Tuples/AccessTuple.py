e18d7373ad6ad813530eb9f3cdee5d23a0edf22b
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-09 15:53:23 +0600

    Access Tuples

---
+++ basics/Tuples/AccessTuple.py
@@ -0,0 +1,9 @@
+languages =('CSS', 'Java', 'LaTex', 'C#', 'English', 'Marathi', 'Telegu', 'TypeScript', 'Python', 'Ruby', 'HTML', 'C++', 'Bangla')
+print(languages)
+
+print(languages[0])
+print(languages[1])
+print(languages[2])
+print(languages[-1])
+print(languages[-2])
+print(languages[2:6])
