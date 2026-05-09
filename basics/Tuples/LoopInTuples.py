b2f0f324c36766adaaaebf30491e2aa7e8a748f2
MahbubulRishad <mahbubulrishad@gmail.com>
2026-05-09 17:31:43 +0600

    Loop in tuples

---
+++ basics/Tuples/LoopInTuples.py
@@ -0,0 +1,7 @@
+languages = ('CSS', 'Java', 'LaTexx', 'C#', 'English', 'Marathi', 'Telegu', 'TypeScript', 'Python', 'Ruby', 'HTML', 'C++', 'Bangla', 'Chainess')
+
+for language in languages:
+    print(language)
+
+for i in range(len(languages)):
+    print(f"Index: {i} = ", languages[i])
